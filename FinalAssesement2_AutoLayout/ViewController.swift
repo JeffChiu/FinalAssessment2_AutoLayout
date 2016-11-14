4c8a0deff54797637a2f72d1362374c607db4ba9
jeff chiu <zhe.chiu@gmail.com>
2016-11-14 11:59:24 +0800

    1st commit

---
+++ FinalAssesement2_AutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalAssesement2_AutoLayout
+//
+//  Created by Chiu Chih-Che on 2016/11/4.
+//  Copyright © 2016年 Jeff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
